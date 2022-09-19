5ae4621968c68bc0677af559dc3293e0ac131110
dev.Vishnu <2vishnukkumar@gmail.com>
2022-09-19 14:56:53 +0530

    fix: initial-code

---
+++ src/summary/test/SummaryForm.test.js
@@ -0,0 +1,23 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import SummaryForm from "../SummaryForm";
+
+describe("Testing Summary From ", () => {
+  //   test("Checkbox is unchecked by default", () => {
+  //     render(<SummaryForm />);
+  //     const Checkbox = screen.getByRole("checkbox", {
+  //       name: /Terms & Condition/,
+  //     });
+  //     expect(Checkbox).not.toBeChecked();
+  //   });
+  //   test("checkbox when check enabled submit button", () => {
+  //     render(<SummaryForm />);
+  //     const button = screen.getByRole("button", { name: "Confirm order" });
+  //     const checkbox = screen.getByRole("checkbox");
+  //     fireEvent.click(checkbox);
+  //     expect(button).toBeEnabled();
+  //   });
+  //   test("checkbox when uncheck disable submit button", () => {
+  //     render(<SummaryForm />);
+  //     const button = screen.getByRole();
+  //   });
+});
